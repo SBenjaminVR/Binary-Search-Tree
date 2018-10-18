a833bcfbdfd5a31f6ce35dc742397338c45e3b89
Benjamín Valdez Rodríguez <benjamin.vr.1998@gmail.com>
2018-10-18 14:35:29 -0500

    New functions added to the Binary Search Tree
    Added operator ==, copy constructor, mirror, maxWidth and nearstRelative

--- BST.h
+++ BST.h
@@ -10,6 +10,7 @@
 class BST {
     public:
         BST();
+        BST(const BST&treeOne);
         ~BST();
         void add(int data);
         bool search(int data);
@@ -18,6 +19,11 @@ class BST {
         int height();
         int whatLevelamI(int data);
         void ancestors(int data);
+        int count();
+        int nearstRelative(int data1, int data2);
+        bool operator==(BST const &treeOne);
+        void mirror();
+        int maxWidth();
 
     private:
         NodeT *root;
@@ -28,12 +34,37 @@ class BST {
         void inOrder(NodeT *r);
         void postOrder(NodeT *r);
         void libera(NodeT *r);
-        void levelXlevel();
-        int calculateHeight(NodeT *r);
+        void printLeaves(NodeT *r);
+        void printLevelbyLevel();
+        int cuenta(NodeT *r);
+        int altura(NodeT *r);
+        bool compara(NodeT *r, NodeT *r2);
+        void changeMirror(NodeT *r);
+        NodeT* copyTree(NodeT *r, NodeT *originalTree);
 };
 
 BST::BST() {
     root = NULL;
+}
+
+NodeT* BST::copyTree(NodeT *r, NodeT *originalTree) {
+    if (originalTree == NULL) {
+        return NULL;
+    }
+    else {
+        r = new NodeT(originalTree->getData());
+        r->setLeft(copyTree(r->getLeft(), originalTree->getLeft()));
+        r->setRight(copyTree(r->getRight(), originalTree->getRight()));
+        return r;
+    }
+}
+BST::BST(const BST&treeOne) {
+    if (treeOne.root == NULL) {
+        root = NULL;
+    }
+    else {
+        root = copyTree(this->root, treeOne.root);
+    }
 }
 
 void BST::libera(NodeT *r) {
@@ -198,25 +229,35 @@ void BST::postOrder(NodeT *r) {
     }
 }
 
-void BST::levelXlevel() {
-    NodeT *curr = root;
-    queue<NodeT*> treepointer;
-
-    if (curr != NULL) {
-        treepointer.push(curr);
-    }
-    while (!treepointer.empty()) {
-        curr = treepointer.front();
-        treepointer.pop();
-        cout << curr->getData() << " ";
-        if (curr->getLeft() != NULL) {
-            treepointer.push(curr->getLeft());
+void BST::printLeaves(NodeT *r) {
+    if (r != NULL) {
+        if (r->getLeft() == NULL && r->getRight() == NULL) {
+            cout << r->getData() << " ";
         }
-        if (curr->getRight() != NULL) {
-            treepointer.push(curr->getRight());
+        else {
+            printLeaves(r->getLeft());
+            printLeaves(r->getRight());
         }
     }
+}
 
+void BST::printLevelbyLevel() {
+    queue<NodeT*> fila;
+    if (root != NULL) {
+        fila.push(root);
+        NodeT *curr;
+        while (!fila.empty()) {
+            curr = fila.front();
+            fila.pop();
+            cout << curr->getData() << " ";
+            if (curr->getLeft() != NULL) {
+                fila.push(curr->getLeft());
+            }
+            if (curr->getRight() != NULL) {
+                fila.push(curr->getRight());
+            }
+        }
+    }
 }
 
 void BST::print(int c) {
@@ -233,106 +274,257 @@ void BST::print(int c) {
         case 3:
             postOrder(root);
             break;
+        case 4:
+            printLeaves(root);
+            break;
         case 5:
-            levelXlevel();
+            printLevelbyLevel();
             break;
     }
     cout << endl;
 }
 
-int BST::calculateHeight(NodeT *r) {
-    int left, right;
-
+int BST::altura(NodeT *r) {
     if (r == NULL) {
         return 0;
     }
-    left = calculateHeight(r->getLeft());
-    right = calculateHeight(r->getRight());
-
-    if (left > right) {
-        return left+1;
-    }
-    else {
-        return right+1;
-    }
-
-    return 0;
+    int left = altura(r->getLeft());
+    int right = altura(r->getRight());
+    return 1 + (left > right ? left : right);
 }
 
 int BST::height() {
-    if (root == NULL) {
-        return 0;
-    }
-    return calculateHeight(root);
+    return altura(root);
 }
 
 int BST::whatLevelamI(int data) {
     NodeT *curr = root;
-    int level = 0;
-
-    if (root == NULL) {
-        return -1;
+    int nivel = 0;
+    while (curr!= NULL) {
+        if (curr->getData() == data) {
+            return nivel;
+        }
+        nivel++;
+        curr = (curr->getData() > data) ? curr->getLeft() : curr->getRight();
     }
-
-    while (curr->getData() != data) {
-        if (curr->getData() < data) {
-            curr = curr->getRight();
-        }
-        else {
-            curr = curr->getLeft();
-        }
-        level++;
-
-        if (curr == NULL) {
-        return -1;
-        }
-    }
-    return level;
+    return -1;
 }
 
 void BST::ancestors(int data) {
-    NodeT *curr = root;
-    stack<int> family;
-    bool check = false;
+    NodeT* curr = root;
+    stack<int> pila;
+    while (curr != NULL) {
+        if (curr->getData() == data) {
+            if (pila.empty()) {
+                cout << "NO EXISTEN ANCESTROS" << endl;
+            }
+            else {
+                while (!pila.empty()) {
+                    cout << pila.top() << " ";
+                    pila.pop();
+                }
+                cout << endl;
+            }
+            return;
+        }
+        pila.push(curr->getData());
+        curr = (curr->getData() > data) ? curr->getLeft() : curr->getRight();
+    }
+    cout << "EL DATO NO EXISTE";
+}
 
-    if (root == NULL) {
-        cout << "Esta vacio";
+int BST::cuenta(NodeT *r) {
+    if (r == NULL) {
+        return 0;
+    }
+    return 1 + cuenta(r->getLeft()) + cuenta(r->getRight());
+}
+
+int BST::count() {
+    return cuenta(root);
+}
+
+bool BST::compara(NodeT *r, NodeT *r2) {
+    if (r != NULL) {
+        if (r->getData() != r2->getData()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool BST::operator==(const BST &treeOne) {
+    NodeT *curr = root, *otherCurr = treeOne.root;
+    int treeOnesize, treeTwosize;
+    queue<NodeT*> myQueueOne;
+    queue<NodeT*> myQueueTwo;
+
+    if (curr == NULL && otherCurr == NULL) {
+        return true;
+    }
+
+    if (root != NULL && otherCurr != NULL) {
+        myQueueOne.push(curr);
+        myQueueTwo.push(otherCurr);
+
+        while (!myQueueOne.empty() && !myQueueTwo.empty()) {
+            treeOnesize = myQueueOne.size();
+            treeTwosize = myQueueTwo.size();
+
+            if (treeOnesize != treeTwosize) {
+                return false;
+            }
+
+            while (treeOnesize > 0 && treeTwosize > 0) {
+                curr = myQueueOne.front();
+                myQueueOne.pop();
+
+                otherCurr = myQueueTwo.front();
+                myQueueTwo.pop();
+
+                if (curr->getData() != otherCurr->getData()) {
+                    return false;
+                }
+
+                if (curr->getLeft() != NULL) {
+                myQueueOne.push(curr->getLeft());
+                }
+                if (curr->getRight() != NULL) {
+                myQueueOne.push(curr->getRight());
+                }
+
+                if (otherCurr->getLeft() != NULL) {
+                myQueueTwo.push(otherCurr->getLeft());
+                }
+                if (otherCurr->getRight() != NULL) {
+                myQueueTwo.push(otherCurr->getRight());
+                }
+
+                treeOnesize--;
+                treeTwosize--;
+            }
+
+        }
+    }
+    else {
+        return false;
+    }
+
+    return true;
+}
+
+int BST::nearstRelative(int data1, int data2) {
+    NodeT *curr = root, *father = NULL;
+    while (curr != NULL) {
+        if ((curr->getData() < data1 && curr->getData() > data2) || (curr->getData() > data1 && curr->getData() < data2)) {
+            return curr->getData();
+        }
+        if (curr->getData() != data1 && curr->getData() != data2) {
+            father = curr;
+        }
+        else {
+            return father->getData();
+        }
+
+        //Preguntar si te dan la root, y si no existe
+
+        if (curr->getData() < data1 && curr->getData() < data2) {
+            curr = curr->getRight();
+        }
+        else if (curr->getData() > data1 && curr->getData() > data2){
+            curr = curr->getLeft();
+        }
+        else {
+            break;
+        }
+    }
+    return -1;
+}
+
+void BST::mirror() {
+    NodeT *curr = root, *temp;
+    queue<NodeT*> myQueue;
+
+    if (curr == NULL) {
         return;
     }
 
-    while (curr->getData() != data) {
-        family.push(curr->getData());
-        if (curr->getData() < data) {
-            if (curr->getRight() != NULL)
-                curr = curr->getRight();
-            else {
-                check = true;
-                break;
-            }
+    myQueue.push(curr);
+    while(!myQueue.empty()) {
+        curr = myQueue.front();
+        myQueue.pop();
+
+        temp = curr->getLeft();
+        curr->setLeft(curr->getRight());
+        curr->setRight(temp);
+
+        if (curr->getLeft() != NULL) {
+            myQueue.push(curr->getLeft());
         }
-        else {
-            if (curr->getLeft() != NULL)
-                curr = curr->getLeft();
-            else {
-                check = true;
-                break;
-            }
+        if (curr->getRight() != NULL) {
+            myQueue.push(curr->getRight());
         }
-
-    }
-
-    if (check) {
-        cout << "No existe el dato";
-    }
-
-    if (family.size() == 0) {
-        cout << "No tiene ancestros";
-    }
-
-    while (!family.empty() && !check) {
-        cout << family.top() << " ";
-        family.pop();
     }
 }
+
+int BST::maxWidth() {
+    int maxNodes = 0, levelNodes = 0;
+    queue<NodeT*> fila;
+    if (root != NULL) {
+        fila.push(root);
+        NodeT *curr;
+        while (!fila.empty()) {
+            levelNodes = fila.size();
+
+            if (levelNodes > maxNodes) {
+                maxNodes = levelNodes;
+            }
+
+            while (levelNodes > 0) {
+                curr = fila.front();
+                fila.pop();
+
+                if (curr->getLeft() != NULL) {
+                fila.push(curr->getLeft());
+                }
+                if (curr->getRight() != NULL) {
+                fila.push(curr->getRight());
+                }
+                levelNodes--;
+            }
+
+        }
+    }
+    return maxNodes;
+}
+
+//LUNES
+//Copy constructor
+// Simular el recorrido en preorden sin usar el add
+// Usar recursividad
+
+//operador ==
+// Recorrido en preorden de los 2 apuntadores, si en un momento son diferentes adios
+
+// Mirror
+// Voltearlo, pruebalo y borrarlo al final
+
+// nearstRelative
+// Si uno de los dos no existe, pues no, si son como padre e hijo, regresa el abuelo (el ancestro mas cercano)
+
+// maxWidht
+// recorrido nivel por nivel y checar cuando hay cambio de nivel
+
+// MIERCOLES
+//isBalanced
+// Cual es la altura de mi arbol izquierdo, cual es la altura del derecho, si la diferencia es menor de 1, adios
+
+//diamater
+// Regresar la longitud maxima (Esta facil si entiendes el de altura) (Regresar un entero con la altura)
+// No esperen esto en el examen
+
+//isBalanced
+// es parte de lo que vamos a ver el martes
+//
 
 #endif // BST_H_INCLUDED
